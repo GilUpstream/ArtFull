5612ae9f40adf0caa0ebd059c3a9d92fef0b5d3b
Kathryn Beaty <kabeaty@gmail.com>
2014-03-24 16:31:52 -0500

    Initial commit with augmented .gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ArtFull::Application.config.secret_key_base = '1ca4647562ff3c1c7a81585846b1684c54486c5144b1fadced095ff126740f6c9892a700b8add18adde19a9e4e9e9b07cb495b20a3c8a7008f719bf8bcb89da6'
