8cec98bc036c3b50430617d80998e60db06fa7bc
Kathryn Beaty <kabeaty@gmail.com>
2014-03-26 15:02:27 -0500

    Generate Relationship join model and add its relationships

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140325202106) do
+ActiveRecord::Schema.define(version: 20140326195923) do
 
   create_table "check_ins", force: true do |t|
     t.integer  "user_id"
@@ -48,6 +48,13 @@ ActiveRecord::Schema.define(version: 20140325202106) do
     t.string   "artist"
     t.string   "type"
     t.string   "time_period"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "relationships", force: true do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
