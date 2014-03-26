5b9f2ae53470e798e374c2ff0db793eaf65a7d90
Kathryn Beaty <kabeaty@gmail.com>
2014-03-26 15:15:07 -0500

    Add User relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ActiveRecord::Base
 
   has_many :pieces, through: :check_ins
   has_many :check_ins
+
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :reverse_relationships, foreign_key: "followed_id", class_name: "Relationship"
+  has_many :followers, through: :reverse_relationships, source: :follower
 end
