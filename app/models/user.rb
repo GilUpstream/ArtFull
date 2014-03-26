d1a81f15076dad617d312fb7424b8b2f3a1545b8
Gilad Salmon <Gilad@Gilads-MacBook-Pro.local>
2014-03-25 19:24:29 -0500

    Add tests for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
+  validates :fname, presence: true, length: { minimum: 2, maximum: 25}, format: { with: /\A[a-zA-Z]+\z/, message: "How about giving us only letters..." }
+  validates :lname, presence: true, length: { minimum: 2, maximum: 25 }, format: { with: /\A[a-zA-Z]+\z/, message: "How about giving us only letters..." }
+  validates :username, presence: true, length: { minimum: 5 }, uniqueness: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
