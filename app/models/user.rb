be8b5b9daa82c4be9c84068bd4659a68c2949344
Kathryn Beaty <kabeaty@gmail.com>
2014-03-26 16:04:32 -0500

    Add user follow methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,16 @@ class User < ActiveRecord::Base
   has_many :followed_users, through: :relationships, source: :followed
   has_many :reverse_relationships, foreign_key: "followed_id", class_name: "Relationship"
   has_many :followers, through: :reverse_relationships, source: :follower
+
+  def following?(other_user)
+    relationships.find_by(followed_id: other_user.id) ? true : false
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by(followed_id: other_user.id).destroy
+  end
 end
